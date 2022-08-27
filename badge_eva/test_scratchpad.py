a807d3ab741c70e7bc7820fd43aeb6a34ad117ac
Jinjiro-San <jinjirosan@users.noreply.github.com>
2022-08-27 14:21:18 +0200

    test02

--- badge_eva/test_scratchpad.py
+++ badge_eva/test_scratchpad.py
@@ -1,6 +1,7 @@
 import badger2040
 import badger_os
 import time
+#from threading import Thread
 
 # Create a new Badger and set it to update FAST
 display = badger2040.Badger2040()
@@ -10,3 +11,13 @@ display.rectangle(100, 45, 112, 30)
 display.pen(15)
 display.text("KLAAR", 100, 60, 1.2)
 display.update()
+
+dtm = time.time()
+pulse = 0.5/Hz
+while True:
+    dtm += pulse
+    time.sleep(dtm - time.time())
+    # LED ON
+    dtm += pulse
+    time.sleep(dtm - time.time())
+    # LED OFF
