2703a809048d8473bcd6cf2a89eddd584c1dbf9b
Jinjiro-San <jinjirosan@users.noreply.github.com>
2024-04-15 10:27:33 +0200

    v2.2-refactor 0.1

--- badge_papa/badge.py
+++ badge_papa/badge.py
@@ -1,6 +1,16 @@
+# Badge Platform papa - hardware platform v3.0
+# (2022-2024)
+#
+# badge.py : v2.2-refactor 0.1
+
 import time
 import badger2040
 import badger_os
+from machine import UART
+from machine import Pin
+
+# Initialize serial Port
+lora = UART(0,baudrate = 9600,tx = Pin(0),rx = Pin(1))
 
 # Global Constants
 WIDTH = badger2040.WIDTH
@@ -27,18 +37,27 @@ RP2040
 E ink
 296x128px"""
 
-BADGE_IMAGE = bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
+# Time delay in seconds between consecutive button press checks
+BUTTON_PRESS_DELAY = 0.2
 
-try:
-    open("badge-image.bin", "rb").readinto(BADGE_IMAGE)
-except OSError:
+# Flag to keep track of which badge image to display
+is_qr_image = False
+
+# Load the badge images
+#BADGE_IMAGE = bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
+#BADGE_IMAGE_QR = bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
+def load_image(filename):
     try:
-        import badge_image
-        BADGE_IMAGE = bytearray(badge_image.data())
-        del badge_image
-    except ImportError:
-        pass
+        with open(filename, "rb") as file:
+            return bytearray(file.read())
+    except OSError:
+        return None
 
+BADGE_IMAGE = load_image("badge-image.bin") or bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
+BADGE_IMAGE_QR = load_image("badge-image-QR.bin") or None
+
+# Load the initial badge image
+CURRENT_BADGE_IMAGE = BADGE_IMAGE
 
 # ------------------------------
 #      Utility functions
@@ -54,6 +73,15 @@ def truncatestring(text, text_size, width):
             text += ""
             return text
 
+# Function to toggle between badge images
+def toggle_badge_image():
+    global is_qr_image, CURRENT_BADGE_IMAGE
+    is_qr_image = not is_qr_image
+    CURRENT_BADGE_IMAGE = BADGE_IMAGE_QR if is_qr_image else BADGE_IMAGE
+    # Explicitly call the draw function here might help to ensure that changes are immediately reflected.
+    draw_badge()
+
+
 
 # ------------------------------
 #      Drawing functions
@@ -65,7 +93,15 @@ def draw_badge():
     display.clear()
 
     # Draw badge image
-    display.image(BADGE_IMAGE, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
+#    if is_qr_image:
+#        # Draw QR code image
+#        display.image(BADGE_IMAGE_QR, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
+#    else:
+#        # Draw badge image
+#        display.image(BADGE_IMAGE, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
+
+    # Draw badge image
+    display.image(CURRENT_BADGE_IMAGE, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
 
     # Draw a border around the image
     display.pen(0)
@@ -125,6 +161,48 @@ def draw_badge():
     display.thickness(2)
     display.text(detail2_text, LEFT_PADDING + name_length + DETAIL_SPACING, HEIGHT - (DETAILS_HEIGHT // 2), DETAILS_TEXT_SIZE)
 
+# ------------------------------
+#        Sigfox functions
+# ------------------------------
+
+def SigfoxInfo():        
+    sf_info = dict();
+    print("Get Status - should be OK")
+    lora.write("AT\r\n")      # Write AT Command
+    time.sleep(2)
+    sf_status = lora.read(2)         # Response Should be OK
+    sf_info['Status'] = sf_status
+    print(sf_status)
+
+    print("Get ID")
+    lora.write("AT$I=10\r\n") # Send Command to Get ID
+    time.sleep(2)
+    sf_id = lora.read(10)
+    sf_info['ID'] = sf_id
+    print(sf_id)
+
+    print("Get PAC")
+    lora.write("AT$I=11\r\n") # Send Command to Get ID
+    time.sleep(2)
+    sf_pac = lora.read(18)
+    sf_info['PAC'] = sf_pac
+    print(sf_pac)
+    
+    return sf_info
+    
+def SigfoxSend():
+    # Initiate a Transmission
+    print("Init Transmission")
+    time.sleep(1)
+    lora.write("AT$RC\r\n") # Send Command to Reset Macro Channels
+    time.sleep(2)
+    data=lora.read(4)
+    print(data)
+    lora.write("AT$SF=AABBCCDD\r\n")  # sends a test string "AABBCCDD"
+    time.sleep(6)
+    data=lora.read(4)        # We should get a OK response
+    print(data)
+
 
 # ------------------------------
 #        Program setup
@@ -171,32 +249,45 @@ detail2_text = truncatestring(detail2_text, DETAILS_TEXT_SIZE,
 draw_badge()
 
 while True:
-    if display.pressed(badger2040.BUTTON_A):
-        badger_os.warning(display, "je hebt op A gedrukt. Dit scherm gaat na 4 secs weg")
-        time.sleep(4)
-        draw_badge()
+    #if display.pressed(badger2040.BUTTON_A):
+        #badger_os.warning(display, "je hebt op A gedrukt. Dit scherm gaat na 4 secs weg")
+        #time.sleep(4)
+        #draw_badge()
 
-    if display.pressed(badger2040.BUTTON_B):
-        badger_os.warning(display, "je hebt op B gedrukt. Dit scherm gaat na 4 secs weg")
-        time.sleep(4)
-        draw_badge()
+    #if display.pressed(badger2040.BUTTON_B):
+        #badger_os.warning(display, "je hebt op B gedrukt. Dit scherm gaat na 4 secs weg")
+        #time.sleep(4)
+        #draw_badge()
 
-    if display.pressed(badger2040.BUTTON_C):
-        badger_os.warning(display, "je hebt op C gedrukt. Dit scherm gaat na 4 secs weg")
-        time.sleep(4)
-        draw_badge()
+    #if display.pressed(badger2040.BUTTON_C):
+        #badger_os.warning(display, "je hebt op C gedrukt. Dit scherm gaat na 4 secs weg")
+        #time.sleep(4)
+        #draw_badge()
 
     if display.pressed(badger2040.BUTTON_UP):
-        badger_os.warning(display, "je hebt op pijl omhoog gedrukt. Dit scherm gaat na 4 secs weg")
-        time.sleep(4)
-        draw_badge()
+        # Toggle between badge images
+        toggle_badge_image()
 
+        # Redraw the badge with the updated image
+        draw_badge()        
+
+        # Introduce a small delay to prevent consecutive button presses from being detected as one
+        time.sleep(BUTTON_PRESS_DELAY)
+        
     if display.pressed(badger2040.BUTTON_DOWN):
-        badger_os.warning(display, "je hebt op pijl omlaag gedrukt. Dit scherm gaat na 4 secs weg")
-        time.sleep(4)
-        draw_badge()
+        # Toggle between badge images
+        toggle_badge_image()
 
+        # Redraw the badge with the updated image
+        draw_badge()     
+
+        # Introduce a small delay to prevent consecutive button presses from being detected as one
+        time.sleep(BUTTON_PRESS_DELAY)
+        
     display.update()
+
+#	Sending an invisible ping to Sigfox everytime the badge is started.
+    SigfoxSend()
 
     # If on battery, halt the Badger to save power, it will wake up if any of the front buttons are pressed
     display.halt()
